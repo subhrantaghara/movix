cf462c50fdf13e462a93521e2af526fa4cbdf2d1
Subhranta04122002 <subhrantaghara@gmail.com>
2024-05-31 23:36:03 +0530

    error fixed

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 import "./style.scss";
 
-const pageNotFound = () => {
-  return (
-    <div>pageNotFound</div>
-  )
-}
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 
-export default pageNotFound
+const PageNotFound = () => {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default PageNotFound;
